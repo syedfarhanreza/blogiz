1e0913848dd6074faa0c503f61bf04e8615826e0
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-09-29 12:06:24 +0600

    loading

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,11 +1,15 @@
+"use client"
 import BlogCard from "@/components/ui/BlogCard";
+import { useGetBlogsQuery } from "@/redux/api/baseApi";
 import { Blog } from "@/types";
 
-const BlogsPage = async () => {
-    const res = await fetch('http://localhost:5000/blogs',{
-        cache: "no-store"
-    });
-    const blogs = await res.json();
+const BlogsPage = () => {
+    // const res = await fetch('http://localhost:5000/blogs',{
+    //     cache: "no-store"
+    // });
+    // const blogs = await res.json();
+    const {data: blogs, isLoading, isError, error} = useGetBlogsQuery("");
+    console.log(blogs);
     return (
         <div className="w-[90%] mx-auto">
              <h1 className='text-4xl text-center my-5'>Latest Blogs from <span className='text-accent'>Blogiz</span></h1>
@@ -15,7 +19,7 @@ const BlogsPage = async () => {
             </p>
             <div className="grid grid-cols-3 gap-4 my-5">
                 {
-                    blogs.map((blog:Blog)=> (<BlogCard key={blog.id} blog={blog}></BlogCard>))
+                    blogs?.map((blog:Blog)=> (<BlogCard key={blog.id} blog={blog}></BlogCard>))
                 }
             </div>
         </div>
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000' }),
   endpoints: (builder) => ({
     getBlogs: builder.query({
       query: () => "/blogs",
